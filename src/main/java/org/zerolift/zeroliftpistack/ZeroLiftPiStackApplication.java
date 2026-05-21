863e4b5551ca3b1d2a83795c1507896c41456385
Julian <230011@o365.htl-leoben.at>
2026-05-21 09:03:57 +0200

    Inital commit

---
+++ src/main/java/org/zerolift/zeroliftpistack/ZeroLiftPiStackApplication.java
@@ -0,0 +1,13 @@
+package org.zerolift.zeroliftpistack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroLiftPiStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroLiftPiStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerolift/zeroliftpistack/ZeroLiftPiStackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerolift.zeroliftpistack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroLiftPiStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
